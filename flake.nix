6c870d162895c973a71b0044db953e610f6059b8
Shogo Takata <shogo-takata@st.go.tuat.ac.jp>
2026-04-22 12:26:08 +0900

    update flake

--- flake.nix
+++ flake.nix
@@ -1,92 +1,66 @@
 {
-  description = "A basic shell";
+  description = "prometheus switchbot prober";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    systems.url = "github:nix-systems/default";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      systems,
-      treefmt-nix,
-    }:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
-      pkgsFor = system: import nixpkgs { inherit system; };
-    in
-    {
-      devShells = eachSystem (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
         {
-          default = pkgs.mkShell {
-            packages = [
-              (pkgs.python3.withPackages (ps: [
-                ps.fastapi
-                ps.jinja2
-                ps.requests
-                ps.uvicorn
-              ]))
-            ];
-          };
-        }
-      );
+          packages = rec {
+            default = docker;
+            docker = pkgs.dockerTools.streamLayeredImage {
+              name = "prometheus-switchbot";
+              tag = "latest";
 
-      packages = eachSystem (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
-        rec {
-          default = docker;
-          docker = pkgs.dockerTools.streamLayeredImage {
-            name = "prometheus-switchbot";
-            tag = "latest";
-
-            contents = [
-              (pkgs.python3.withPackages (ps: [
-                ps.fastapi
-                ps.jinja2
-                ps.requests
-                ps.uvicorn
-              ]))
-              (pkgs.runCommand "app-dir" { } ''
-                mkdir -p $out/app
-                cp -r ${./src} $out/app/src
-              '')
-            ];
-
-            config = {
-              Cmd = [
-                "uvicorn"
-                "--host"
-                "0.0.0.0"
-                "src.main:app"
+              contents = [
+                (pkgs.python3.withPackages (ps: [
+                  ps.fastapi
+                  ps.jinja2
+                  ps.requests
+                  ps.uvicorn
+                ]))
+                (pkgs.runCommand "app-dir" { } ''
+                  mkdir -p $out/app
+                  cp -r ${./src} $out/app/src
+                '')
               ];
-              WorkingDir = "/app";
+
+              config = {
+                Cmd = [
+                  "uvicorn"
+                  "--host"
+                  "0.0.0.0"
+                  "src.main:app"
+                ];
+                WorkingDir = "/app";
+              };
             };
           };
 
-        }
-      );
+          devShells = {
+            default = pkgs.mkShell {
+              packages = [
+                (pkgs.python3.withPackages (ps: [
+                  ps.fastapi
+                  ps.jinja2
+                  ps.requests
+                  ps.uvicorn
+                ]))
+              ];
+            };
+          };
 
-      formatter = eachSystem (
-        system:
-        (treefmt-nix.lib.evalModule (pkgsFor system) {
-          projectRootFile = "flake.nix";
-          programs.nixfmt.enable = true;
-        }).config.build.wrapper
-      );
-
-      legacyPackages = eachSystem pkgsFor;
+          # Use nixfmt for all nix files
+          formatter = pkgs.nixfmt-tree;
+        };
     };
 }
